1c4d0cb281d7ccbd55c0d4131f8682a73a36cb0b
Shibu Thannikkunnath <shibu@hotmail.co.in>
2019-11-27 00:40:11 -0500

    successful docker build

---
+++ dockerfile
@@ -0,0 +1,18 @@
+FROM mcr.microsoft.com/dotnet/core/sdk:2.2 AS build-env
+WORKDIR /app
+
+# Copy projetct and restore
+COPY *.sln .
+COPY netcoredocker/*.csproj ./netcoredocker/
+RUN dotnet restore
+
+# Copy everything else and build
+COPY netcoredocker/. ./netcoredocker/
+WORKDIR /app/netcoredocker
+RUN dotnet publish -c Release -o out
+
+# Build runtime image
+FROM mcr.microsoft.com/dotnet/core/aspnet:2.2 AS runtime
+WORKDIR /app/
+COPY --from=build-env /app/netcoredocker/out .
+ENTRYPOINT ["dotnet", "netcoredocker.dll"]
--- netcoredocker/Startup.cs
+++ netcoredocker/Startup.cs
@@ -1,16 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace netcoredocker
 {
@@ -26,31 +17,13 @@ namespace netcoredocker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .AddNewtonsoftJson();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting(routes =>
-            {
-                routes.MapControllers();
-            });
-
-            app.UseAuthorization();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
